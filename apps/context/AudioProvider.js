6e3a6d52d9f255b940e6c0fdeb81ddbeb3cebcce
samirhassen <hassen.samir@gmail.com>
2021-08-26 12:16:00 +0200

    half way audio player

--- apps/context/AudioProvider.js
+++ apps/context/AudioProvider.js
@@ -1,3 +1,236 @@
+// Play Music / Sound in React Native App for Android and iOS
+// https://aboutreact.com/react-native-play-music-sound/
+
+// import React in our code
+import React, {useEffect} from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from 'react-native';
+
+// import Sound Component
+import Sound from 'react-native-sound';
+
+const App = () => {
+  let sound1, sound2, sound3, sound4, sound5, sound6;
+
+  useEffect(() => {
+    Sound.setCategory('Playback', true); // true = mixWithOthers
+    return () => {
+      if (sound1) sound1.release();
+      if (sound2) sound2.release();
+      if (sound3) sound3.release();
+      if (sound4) sound4.release();
+      if (sound5) sound5.release();
+      if (sound6) sound6.release();
+    };
+  }, []);
+
+  //List of the dummy sound track
+  const audioList = [
+    {
+      title: 'Bone',
+      isRequire: true,
+      url: require('../../assets/Bone.wav'),
+    },
+    {
+      title: 'Circulation',
+      url: require('../../assets/Circulation.wav'),
+    },
+    {
+      title: 'Energize',
+      isRequire: true,
+      url: require('../../assets/Energize.wav'),
+    },
+    {
+      title: 'Focus',
+      url: require('../../assets/Focus.wav'),
+    },
+    {
+      title: 'Inflammation',
+      isRequire: true,
+      url: require('../../assets/Inflammation.wav'),
+    },
+    {
+      title: 'Performance',
+      url: require('../../assets/Performance.wav'),
+    }
+  ];
+
+  const playSound = (item, index) => {
+    if (index == 0) {
+      sound1 = new Sound(item.url, (error, _sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound1.play(() => {
+          sound1.release();
+        });
+      });
+    } else if (index == 1) {
+      sound2 = new Sound(item.url, '', (error, _sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound2.play(() => {
+          sound2.release();
+        });
+      });
+    } else if (index == 2) {
+      sound3 = new Sound(item.url, (error, _sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound3.play(() => {
+          sound3.release();
+        });
+      });
+    } else if (index == 3) {
+      sound4 = new Sound(item.url, '', (error, _sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound4.play(() => {
+          sound4.release();
+        });
+      });
+    } else if (index == 4) {
+      sound5 = new Sound(item.url, (error, _sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound5.play(() => {
+          sound5.release();
+        });
+      });
+    } else if (index == 5) {
+      sound6 = new Sound(item.url, '', (error, _sound) => {
+        if (error) {
+          alert('error' + error.message);
+          return;
+        }
+        sound6.play(() => {
+          sound6.release();
+        });
+      });
+    }
+  };
+
+  const stopSound = (_item, index) => {
+    if (index == 0 && sound1) {
+      sound1.stop(() => {
+        console.log('Stop');
+      });
+    } else if (index == 1 && sound2) {
+      sound2.stop(() => {
+        console.log('Stop');
+      });
+    } else if (index == 2 && sound3) {
+      sound3.stop(() => {
+        console.log('Stop');
+      });
+    } else if (index == 3 && sound4) {
+      sound4.stop(() => {
+        console.log('Stop');
+      });
+    } else if (index == 4 && sound5) {
+      sound5.stop(() => {
+        console.log('Stop');
+      });
+    } else if (index == 5 && sound6) {
+      sound6.stop(() => {
+        console.log('Stop');
+      });
+    }
+  };
+
+  const ItemView = (item, index) => {
+    return (
+      <View style={styles.feature} key={index}>
+        <Text style={styles.textStyle}>{item.title}</Text>
+        <TouchableOpacity onPress={() => playSound(item, index)}>
+          <Text style={styles.buttonPlay}>Play</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => stopSound(item, index)}>
+          <Text style={styles.buttonStop}>Stop</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          Play Music / Sound in React Native App for Android and iOS
+        </Text>
+        <ScrollView style={{flex: 1}}>
+          {audioList.map(ItemView)}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  textStyle: {
+    flex: 1,
+    padding: 5,
+  },
+  buttonPlay: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'rgba(00,80,00,1)',
+    borderWidth: 1,
+    borderColor: 'rgba(80,80,80,0.5)',
+    overflow: 'hidden',
+    paddingHorizontal: 15,
+    paddingVertical: 7,
+  },
+  buttonStop: {
+    fontSize: 16,
+    color: 'white',
+    backgroundColor: 'rgba(80,00,00,1)',
+    borderWidth: 1,
+    borderColor: 'rgba(80,80,80,0.5)',
+    overflow: 'hidden',
+    paddingHorizontal: 15,
+    paddingVertical: 7,
+  },
+  feature: {
+    flexDirection: 'row',
+    padding: 5,
+    marginTop: 7,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: 'rgb(180,180,180)',
+  },
+});
+/*
 import React, { Component, createContext } from 'react';
 import { Text, View, Alert } from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
@@ -274,3 +507,4 @@ export class AudioProvider extends Component {
 }
 
 export default AudioProvider;
+*/
