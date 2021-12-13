c75cbc616a3eab2172557bc029f46c48402c63b0
Gupta <akshay.a.gupta@capgemini.com>
2021-12-13 16:40:47 +0530

    ficed permission for ios

--- apps/components/AudioListItem.js
+++ apps/components/AudioListItem.js
@@ -76,7 +76,10 @@ const AudioListItem = ({
   const [loader, setLoader] = useState(false);
   const {getAudioFiles } = useContext(AudioContext);
   const saveFile = async (fileUri) => {
-    const { status } = await Audio.requestPermissionsAsync();
+    const { status } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.MEDIA_LIBRARY
+    );
     if (status === 'granted') {
       const asset = await MediaLibrary.createAssetAsync(fileUri);
       await MediaLibrary.createAlbumAsync('octaCoil', asset, true);
