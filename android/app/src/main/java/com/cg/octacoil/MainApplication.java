fa49aee0733a882b3a09721fa5c62c2cca3ab6c1
Talwinder Singh <talwindersingh@TalwindersMini2.localdomain>
2022-12-22 16:18:16 +0530

    Issues fixed

--- android/app/src/main/java/com/cg/octacoil/MainApplication.java
+++ android/app/src/main/java/com/cg/octacoil/MainApplication.java
@@ -19,6 +19,7 @@ import com.facebook.react.bridge.JSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.brentvatne.react.ReactVideoPackage;
 
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
@@ -34,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      packages.add(new ReactVideoPackage());
       return packages;
     }
 
